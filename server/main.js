3699387a7b5444af83dcfdf811c83d50b5f5ac1c
Tudor Lapuste <tlapuste@gmail.com>
2016-02-01 15:29:35 -0800

    using express.static

--- server/main.js
+++ server/main.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+// server represents instance of express wrapped in 'http'
 var server = require('http').Server(app);
+// 'socket.io' instance called with http-wrapped express
+var io = require('socket.io')(server);
 
-app.get('/', function(req,res) {
-  res.send("Hello world.");
-  console.log("GET-handler for express root.")
-});
+// app.get('/', function(req,res) {
+//   res.send("Hello world.");
+//   console.log("GET-handler for express root.")
+// });
 
-server.listen(80);
+app.use(express.static('app'));
+
+// wait to run until something to connect
+io.on('connection', function(socket) {
+  console.log("Something connected to Socket.io");
+  // emit messages
+  socket.emiti("messages", ["Hello", "Hi there", "How are you?"]);
+
+})
+
+// Note that server i used here instead of app
+server.listen(8080);

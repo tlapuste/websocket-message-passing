4e2373a740b3ca2465423605c5305ce3e5d3e956
Tudor Lapuste <tlapuste@gmail.com>
2016-02-01 16:09:42 -0800

    server-side rendering initial page load

--- app/main.js
+++ app/main.js
@@ -5,4 +5,15 @@ var socket = io.connect('http://localhost:8080', {'forceNew': true});
 
 socket.on("messages", function(data) {
   console.info(data);
+  var html = data.map(function(data){
+    return (`
+            <div class='name'>
+              ${data.userName}
+            </div>
+            <a href=${data.content.link} class='message' target=blank>
+              ${data.content.text}
+            </a>
+    `)
+  }).join(" ");
+  document.getElementById("messages").innerHTML = html;
 });
